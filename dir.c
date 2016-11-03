3829da231b4f4d5bf575b2449ac364692bc974ff
Leon Loi <leonloi3278@gmail.com>
2016-11-02 22:57:18 -0400

    clean code kappa

--- dir.c
+++ dir.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/types.h>
-
+#include <sys/stat.h>
 
 void main(){
+  struct stat st;
   DIR * dir;
   struct dirent * d;
   int size;
   if((dir = opendir(".")) != NULL){
     while(d = readdir(dir)){
-      size+=
-      if((d->d_type) == DT_DIR) printf("%s: directory\n" , d->d_name);
-      else printf("%s\n" , d->d_name);
+      stat(d->d_name, &st);
+      size += st.st_size;
+      if((d->d_type) == DT_DIR) printf("%s: directory %d bytes\n" , d->d_name , st.st_size);
+      else printf("%s: regular file %d bytes\n" , d->d_name,st.st_size);
     }
   }
+  printf("size of directory %d\n", size);
   closedir(dir);
 }
